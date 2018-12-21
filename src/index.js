8d4e7c95ed8a8aea8cc6bd25b741c88567de4e26
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-21 12:39:11 -0700

    uncomment <Toggle /> comp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import ToggleButton from './components/ToggleButton'
+import ToggleButton from './components/ToggleButton'
 import Address from './components/Address'
 
 ReactDOM.render(
